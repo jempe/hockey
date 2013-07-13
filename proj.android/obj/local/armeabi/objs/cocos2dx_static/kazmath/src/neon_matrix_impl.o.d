7b72b0228702284f4cbba385b6a022d518297937
Sucio Kastro <suciokastro@gmail.com>
2013-07-13 11:34:04 -0500

    court graphics fix + cocos2dx update

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o: \
- /home/sucio/bin/cocos2dx2.1/Hockey/proj.android/../../cocos2dx/kazmath/src/neon_matrix_impl.c \
- /home/sucio/bin/cocos2dx2.1/Hockey/proj.android/../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h
-
-/home/sucio/bin/cocos2dx2.1/Hockey/proj.android/../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h:
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- /home/sucio/bin/cocos2dx2.1/Hockey/proj.android/../../cocos2dx/support/ccUtils.cpp \
- /home/sucio/bin/cocos2dx2.1/Hockey/proj.android/../../cocos2dx/support/ccUtils.h
-
-/home/sucio/bin/cocos2dx2.1/Hockey/proj.android/../../cocos2dx/support/ccUtils.h:
